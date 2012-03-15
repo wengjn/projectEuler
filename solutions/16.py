2e9730ff0573d808056173794dabb75fe63198d0
jiannian weng <jnweng2010@gmail.com>
2012-03-14 22:55:50 -0500

    update 16.py

--- solutions/16.py
+++ solutions/16.py
@@ -1,4 +1,18 @@
 #! /usr/bin/python
 # filename: 16.py
 
-#
+# python is really powerful
+
+def main():
+  summ = 0
+  text = str(2**1000)
+
+  for i in range(len(text)):
+    summ += int(text[i])
+
+
+  print summ
+
+
+if __name__=='__main__':
+  main()
